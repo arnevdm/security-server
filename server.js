928f4826b2d81ea0a2828a73dcc32aa568c5aeb8
arnevdm <arnevanimus@gmail.com>
2022-05-15 13:28:57 +0200

    changes

--- server.js
+++ server.js
@@ -7,18 +7,28 @@ const morgan = require('morgan')
 // const bodyParser = require('body-parser'); you no longer need body-parser package...it comes built into expressJS now
 
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(helmet())
 app.use(express.json()) //you no longer need body-parser package...it comes built into expressJS now
 // app.use(morgan('combined'))
 
+var whitelist = ['https://arnevdm.github.io', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.get('/', (req, res) => {
-  res.cookie('session', '1', { httpOnly: true })
-  res.cookie('session', '1', { secure: true })
-res.set({
-  'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
-})
+//   res.cookie('session', '1', { httpOnly: true })
+//   res.cookie('session', '1', { secure: true })
+// res.set({
+//   'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+// })
 res.send('Hello World!')
 });
 
